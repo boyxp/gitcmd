088de2cbed056bab3efc819b1c389e26ac74771e
lee <isme@live.com>
2020-09-25 13:18:01 +0800

    安装

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ ! -f ~/.bash_gitcmd ]; then
+        curl -s https://raw.githubusercontent.com/boyxp/gitcmd/master/.bash_gitcmd -o ~/.bash_gitcmd
+
+        echo "
+if [ -f ~/.bash_gitcmd ]; then
+        . ~/.bash_gitcmd
+fi" >> ~/.bash_profile
+fi
+
